08588f2bc45b08ec254d8b982a96aae4b5e19c8e
dillonwells <dwells10@my.wcyc.edu>
2018-01-30 11:23:27 -0600

    - cat javadoc update

--- src/com/dillon/Cat.java
+++ src/com/dillon/Cat.java
@@ -1,26 +1,61 @@
 package com.dillon;
 
+/** @author Created by Matt Green
+ *
+ * Cat class used for returning the number of mice killed per cat
+ *
+ */
+
 
 public class Cat extends Pet implements Talkable {
+
+    /**
+     * Number of mice Killed
+     */
     private int mousesKilled;
+
+    /**
+     *  This is a constructor to initililize cat
+     * @param mousesKilled the number of mice killed
+     * @param name the name of the cat
+     */
 
     public Cat(int mousesKilled, String name) {
         super(name);
         this.mousesKilled = mousesKilled;
     }
 
+    /**
+     *
+     * @return returns integer of the number of mice killed
+     */
+
     public int getMousesKilled() {
         return mousesKilled;
     }
+
+    /**
+     * Increments the number of mice killed by 1
+     */
 
     public void addMouse() {
         mousesKilled++;
     }
 
+    /**
+     *
+     * Implements the talk method and overrides with Meow
+     */
+
     @Override
     public String talk() {
         return "Meow";
     }
+
+    /**
+     *
+     * @return returns the Cat name and number of mice killed as string
+     */
 
     @Override
     public String toString() {
--- src/com/dillon/test/CatTest.java
+++ src/com/dillon/test/CatTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 public class CatTest {
 
+    Cat tiny = new Cat(5,);
+
     @org.junit.Test
     public void getMousesKilled() {
     }
